ab9cc06a6bfe058bcade9b0e9c2fe17ae56b6977
antonsoroko <anton.soroko@gmail.com>
2016-03-20 22:02:50 +0300

    Added new lines in manual_tests.sh output.

--- manual_tests.sh
+++ manual_tests.sh
@@ -1,4 +1,6 @@
 curl -H "Content-type: application/json" -i http://127.0.0.1:8080/post -X POST --data '[{"date": "2015-05-12T14:36:00.451765", "uid": "1", "md5checksum": "e8c83e232b64ce94fdd0e4539ad0d44f", "name": "John Doe"}, {"date": "2015-05-13T14:36:00.451765", "uid": "2", "md5checksum": "13065eda9a6ab62be1e63276cc7c46b1", "name": "Jane Doe"}]'
+echo
 curl -i 'http://127.0.0.1:8080/get?uid=1&date=2015-05-12T14:36:00.451765'
+echo
 curl -i 'http://127.0.0.1:8080/get?uid=2&date=2015-05-13'
 
